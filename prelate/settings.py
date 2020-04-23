dc5c64b0f6a681642d5aa7ce8debac0308bea3ad
Olamigoke Philip <olamigokayphils@gmail.com>
2020-04-23 15:18:04 +0100

    collect init staticfiles

--- prelate/settings.py
+++ prelate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qf2*(*kb0tqpqjp1cg78)!tt@02qg%x!qxf7@xj(g(291v36*r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
