c7f97029780363f93c447b7990502e67abdd4b70
Olayinka O <omosehintemilade@gmail.com>
2023-08-05 23:23:48 +0100

    [wip] mobile-responsiveness => swiper section for desktop

---
+++ static/javascripts/a.js
@@ -0,0 +1,21 @@
+new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false
+  // }
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
